6888b964af1cea690fc5e2bb5ff29d903479d642
Peter Fačko <pfacko1@gmail.com>
2023-10-14 21:16:55 +0200

    mock runner usage to run a build image

--- PPpackage/PPpackage/sub.py
+++ PPpackage/PPpackage/sub.py
@@ -1,8 +1,18 @@
 from collections.abc import Iterable, Mapping, Set
+from os import environ
 from pathlib import Path
+from sys import stderr
 from typing import Any
 
-from PPpackage_utils.utils import Resolution, frozendict
+from PPpackage_utils.io import (
+    stream_read_line,
+    stream_write_line,
+    stream_write_string,
+    stream_write_strings,
+)
+from PPpackage_utils.utils import MyException, Resolution, TemporaryPipe, frozendict
+
+from .utils import communicate_with_daemon, machine_id_relative_path, read_machine_id
 
 
 async def update_database(debug: bool, cache_path: Path) -> None:
@@ -29,6 +39,67 @@ async def fetch(
     generators: Iterable[str],
     generators_path: Path,
 ) -> Mapping[str, str]:
+    run_path = Path("/") / "run"
+    runner_path = run_path / "PPpackage-runner.sock"
+
+    async with communicate_with_daemon(debug, runner_path) as (
+        runner_reader,
+        runner_writer,
+    ):
+        machine_id = read_machine_id(Path("/") / machine_id_relative_path)
+
+        stream_write_string(debug, "PPpackage-sub", runner_writer, machine_id)
+
+        stream_write_line(debug, "PPpackage-sub", runner_writer, "RUN")
+        stream_write_line(debug, "PPpackage-sub", runner_writer, "IMAGE")
+        stream_write_string(
+            debug, "PPpackage-sub", runner_writer, "docker.io/archlinux:latest"
+        )
+
+        await runner_writer.drain()
+
+        success = await stream_read_line(debug, "PPpackage-sub", runner_reader)
+
+        if success != "SUCCESS":
+            raise MyException("PPpackage-sub: Failed to pull the build image.")
+
+        stream_write_strings(debug, "PPpackage-sub", runner_writer, ["cat", "-"])
+
+        runner_workdir = Path("/") / "mnt" / "PPpackage-runner"
+
+        with TemporaryPipe(runner_workdir) as stdin_pipe_path, TemporaryPipe(
+            runner_workdir
+        ) as stdout_pipe_path:
+            stream_write_string(
+                debug,
+                "PPpackage-sub",
+                runner_writer,
+                str(stdin_pipe_path.relative_to(runner_workdir)),
+            )
+
+            stream_write_string(
+                debug,
+                "PPpackage-sub",
+                runner_writer,
+                str(stdout_pipe_path.relative_to(runner_workdir)),
+            )
+
+            stream_write_strings(debug, "PPpackage-sub", runner_writer, [])
+            stream_write_strings(debug, "PPpackage-sub", runner_writer, [])
+
+            with stdin_pipe_path.open("w") as stdin_pipe:
+                stdin_pipe.write("ahoj!")
+
+            with stdout_pipe_path.open("r") as stdout_pipe:
+                print(stdout_pipe.read(), file=stderr)
+
+        await runner_writer.drain()
+
+        success = await stream_read_line(debug, "PPpackage-sub", runner_reader)
+
+        if success != "SUCCESS":
+            raise MyException("PPpackage-sub: Failed to run the build image.")
+
     product_ids = {name: "id" for name in versions.keys()}
 
     return product_ids
