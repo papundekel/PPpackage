8872bc54c967cb2e8bd1e6115dd2c8052ca73e2c
Peter Fačko <pfacko1@gmail.com>
2023-10-14 21:16:12 +0200

    runner needs a home directory

--- run-docker-runc.sh
+++ run-docker-runc.sh
@@ -2,6 +2,8 @@ runc_path="$1"
 debug="$2"
 name="fackop-pppackage-runner"
 
+mkdir -p "$runc_path/storage"
+
 docker run \
     --privileged \
     --detach \
@@ -9,6 +11,7 @@ docker run \
     --name "$name" \
     --mount type=bind,source="$runc_path/run",destination="/workdir/PPpackage-runner/run" \
     --mount type=bind,source="$runc_path/containers",destination="/workdir/PPpackage-runner/containers" \
+    --mount type=bind,source="$runc_path/storage",destination="/home/runner/.local/share/containers" \
     fackop/pppackage-runner PPpackage-runner PPpackage-runner/ $debug > /dev/null
 
 echo "$name"
