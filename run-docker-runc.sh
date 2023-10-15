b3eb1dc7c13a2763b03126e7faf685a7f7aa26b3
Peter Fačko <pfacko1@gmail.com>
2023-10-15 15:13:57 +0200

    docker -> podman solves the errors

--- run-docker-runc.sh
+++ run-docker-runc.sh
@@ -4,14 +4,13 @@ name="fackop-pppackage-runner"
 
 mkdir -p "$runc_path/storage"
 
-docker run \
+podman run \
     --privileged \
     --detach \
-    --user "$(id -u):$(id -g)" \
     --name "$name" \
     --mount type=bind,source="$runc_path/run",destination="/workdir/PPpackage-runner/run" \
     --mount type=bind,source="$runc_path/containers",destination="/workdir/PPpackage-runner/containers" \
-    --mount type=bind,source="$runc_path/storage",destination="/home/runner/.local/share/containers" \
+    --mount type=bind,source="$runc_path/storage",destination="/var/lib/containers" \
     fackop/pppackage-runner PPpackage-runner PPpackage-runner/ $debug > /dev/null
 
 echo "$name"
