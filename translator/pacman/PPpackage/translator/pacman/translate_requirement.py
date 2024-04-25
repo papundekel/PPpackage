8af7a266969d4404ba4299a7db5413026e7037be
Peter Fačko <pfacko1@gmail.com>
2024-04-25 11:22:25 +0200

    pacman translate refactor

--- translator/pacman/PPpackage/translator/pacman/translate_requirement.py
+++ translator/pacman/PPpackage/translator/pacman/translate_requirement.py
@@ -1,6 +1,5 @@
-from collections.abc import Iterable, Mapping
-from enum import Enum, auto
-from sys import stderr
+from collections.abc import Callable, Iterable, Mapping
+from operator import eq, ge, gt, le, lt
 
 from pyalpm import vercmp as alpm_vercmp
 from pysat.formula import Atom, Formula, Or
@@ -8,68 +7,34 @@ from pysat.formula import Atom, Formula, Or
 from .schemes import Parameters
 
 
-class Comparison(Enum):
-    EQUAL = auto()
-    GREATER = auto()
-    GREATER_EQUAL = auto()
-    LESS = auto()
-    LESS_EQUAL = auto()
-    ANY = auto()
+def parse_requirement(
+    requirement: str,
+) -> tuple[str, tuple[Callable[[int, int], bool], str] | None]:
+    for token, operator in [
+        (">=", ge),
+        ("<=", le),
+        (">", gt),
+        ("<", lt),
+        ("=", eq),
+    ]:
+        match requirement.rsplit(token, 1):
+            case package, version:
+                return package, (operator, version)
 
-
-def parse_requirement(requirement: str) -> tuple[str, Comparison, str]:
-    tokens = requirement.rsplit(">=", 1)
-    if len(tokens) == 2:
-        return tokens[0], Comparison.GREATER_EQUAL, tokens[1]
-
-    tokens = requirement.rsplit("<=", 1)
-    if len(tokens) == 2:
-        return tokens[0], Comparison.LESS_EQUAL, tokens[1]
-
-    tokens = requirement.rsplit(">", 1)
-    if len(tokens) == 2:
-        return tokens[0], Comparison.GREATER, tokens[1]
-
-    tokens = requirement.rsplit("<", 1)
-    if len(tokens) == 2:
-        return tokens[0], Comparison.LESS, tokens[1]
-
-    tokens = requirement.rsplit("=", 1)
-    if len(tokens) == 2:
-        return tokens[0], Comparison.EQUAL, tokens[1]
-
-    return requirement, Comparison.ANY, ""
+    return requirement, None
 
 
 def version_compare(
-    version_left: str, comparison: Comparison, version_right: str
+    version_left: str,
+    comparison: Callable[[int, int], bool],
+    version_right: str,
 ) -> bool:
-    if comparison == Comparison.ANY:
-        return True
-
     if version_left == "":
         return False
 
     cmp = alpm_vercmp(version_left, version_right)
 
-    match comparison:
-        case Comparison.EQUAL:
-            return cmp == 0
-        case Comparison.GREATER:
-            return cmp > 0
-        case Comparison.GREATER_EQUAL:
-            return cmp >= 0
-        case Comparison.LESS:
-            return cmp < 0
-        case Comparison.LESS_EQUAL:
-            return cmp <= 0
-
-
-def make_variable(package: str, version: str) -> str:
-    if version == "":
-        return f"pacman-{package}"
-
-    return f"pacman-{package}-{version}"
+    return comparison(cmp, 0)
 
 
 async def translate_requirement(
@@ -77,15 +42,22 @@ async def translate_requirement(
     data: Mapping[str, Iterable[str]],
     requirement: str,
 ) -> Formula:
-    package, comparison, required_version = parse_requirement(requirement)
+    package, requirement_expression = parse_requirement(requirement)
 
     versions = data.get(f"pacman-{package}", [])
 
     result = Or(
         *(
-            Atom(make_variable(package, version))
+            Atom(
+                f"pacman-{package}-{version}" if version != "" else f"pacman-{package}"
+            )
             for version in versions
-            if version_compare(version, comparison, required_version)
+            if requirement_expression is None
+            or version_compare(
+                version,
+                requirement_expression[0],
+                requirement_expression[1],
+            )
         )
     )
 
