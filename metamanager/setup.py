98d6d202d9a6825bea2d8a63c68a254f38d6ec2b
Peter Fačko <pfacko1@gmail.com>
2024-04-30 11:21:49 +0200

    bugfix: fetch package of metamanager missing in setup.py

--- metamanager/setup.py
+++ metamanager/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="PPpackage-metamanager",
-    packages=["PPpackage.metamanager"],
+    packages=["PPpackage.metamanager", "PPpackage.metamanager.fetch"],
     version="0.1.0",
     install_requires=[
         "PPpackage-utils",
