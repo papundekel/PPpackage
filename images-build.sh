1450736ea731881d22789243708b43889ffd846b
Peter Fačko <pfacko1@gmail.com>
2024-05-15 15:46:39 +0200

    bugfix

--- images-build.sh
+++ images-build.sh
@@ -5,7 +5,6 @@ fi
 
 "$1" build --target repository-driver-pacman --tag docker.io/fackop/pppackage-repository-driver-pacman:latest . && \
 "$1" build --target repository-driver-conan --tag docker.io/fackop/pppackage-repository-driver-conan:latest . && \
-"$1" build --target repository-driver-pp --tag docker.io/fackop/pppackage-repository-driver-pp:latest . &&\
 "$1" build --target repository-driver-aur --tag docker.io/fackop/pppackage-repository-driver-aur:latest . &&\
 "$1" build --target updater --tag docker.io/fackop/pppackage-updater:latest . &&\
 "$1" build --target solver --tag docker.io/fackop/pppackage-solver:latest . &&\
